f1d5f320b2b1af589988b7fab4e590f51e274c34
shehzadasadullah <shehzad.asadullah@qisstpay.com>
2024-02-21 23:39:43 +0500

    update

--- pages/events/index.js
+++ pages/events/index.js
@@ -11,6 +11,10 @@ const Attend = () => {
           name="description"
           content="Circle.ooo ❤️'s our customers! Events: beautiful, fast & simple for all."
         />
+        <meta
+          property="og:image"
+          content="https://firebasestorage.googleapis.com/v0/b/noww-d5ce2.appspot.com/o/circles%2FbannerCircle.png?alt=media&token=c951bb6c-8c98-4e3f-9a30-40b6a5ec65d5"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
